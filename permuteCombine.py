b6c1424f4d53ba629ce27d1e3ef524fcf51efa8d
Sriharsha Gangam <sgangam@purdue.edu>
2013-04-02 23:35:47 -0400

    first file

---
+++ permuteCombine.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+def permute(someList, startIndex):
+    if len(someList) - 1 == startIndex:
+        print someList
+    else:
+        newList = [i for i in someList]
+        permute(someList, startIndex + 1)
+        i = startIndex + 1
+        while i < len(someList):
+            someList = newList
+            first = someList[startIndex]
+            someList[startIndex] = someList[i]
+            someList[i] = first
+            permute(someList, startIndex + 1)
+            i += 1
+
+def combine(someList, startIndex):
+    if len(someList)  == startIndex:
+        print someList
+    else:
+        newList = [i for i in someList]
+        combine(someList, startIndex + 1)
+        someList = newList
+        someList.pop(startIndex)
+        combine(someList, startIndex)
+
+someList = ['a', 'b', 'c', 'd']
+permute(someList, 0)
+print "************************"
+combine(someList, 0)
